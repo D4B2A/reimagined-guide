548d9e95310cbcc62c34b2a2ecae1da3b974e0c9
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-10-14 16:41:55 +0200

    Update error.c

--- src/cores/Arduino/error.c
+++ src/cores/Arduino/error.c
@@ -10,5 +10,6 @@ void catchError(int errorCode, bool fatal){
 void reportError(int destination){
   if (destination & USART) writeUSART(*msgPointer);
   if (destination & I2C) errorI2C(*msgPointer);
-  if (destination & SPI) errorSPI(*msgPointer)
+  if (destination & SPI) errorSPI(*msgPointer);
+  if (destination & EEPROM) writeEEPROM(*msgPointer,0);
 }
