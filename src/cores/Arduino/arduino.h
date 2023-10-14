6d40ae71f01bc9782f09c7580289908df9b8e09f
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-10-14 16:15:19 +0200

    Create arduino.h

---
+++ src/cores/Arduino/arduino.h
@@ -0,0 +1,15 @@
+#define A0 14
+#define A1 15
+#define A2 16
+#define A3 17
+#define A4 18
+#define A5 19
+
+#define INPUT 0
+#define PULL_UP 1
+#define OUTPUT 2
+
+disable_PULL_UP(){}
+
+enable_PULL_UP(){}
+
